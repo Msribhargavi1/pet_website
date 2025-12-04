1cd935eddabc7fbdf8496a3d556a126c4367edbb
Msribhargavi1 <sribhargavi1997@gmail.com>
2025-12-04 18:51:25 +0530

    Remove old Java backend and migrate to Python FastAPI backend

--- backend/src/main/java/com/petcare/repository/PetInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.petcare.repository;
-
-import com.petcare.model.PetInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetInfoRepository extends JpaRepository<PetInfo, Long> {
-}
